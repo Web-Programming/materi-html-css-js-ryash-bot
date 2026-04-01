7c40b8368073a6605f160ad18a98f2a07da94857
Ryansah <rifkiyansah11@gmail.com>
2026-04-01 23:49:39 +0700

    tugas Portofolio

---
+++ Tugas Portofolio/js/js.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  // Scroll ke section
+  window.scrollToSection = function(id) {
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+  };
+
+  const faders = document.querySelectorAll('.fade-in');
+
+  window.addEventListener('scroll', () => {
+    faders.forEach(el => {
+      const rect = el.getBoundingClientRect().top;
+      if (rect < window.innerHeight - 100) {
+        el.classList.add('show');
+      }
+    });
+  });
+
+  const form = document.getElementById("contactForm");
+  if (form) {
+    form.addEventListener("submit", function(e){
+      e.preventDefault();
+      alert("Pesan berhasil dikirim!");
+    });
+  }
+
+  const ctx = document.getElementById('skillsChart');
+  if (ctx) {
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['HTML', 'CSS', 'JavaScript', 'Java'],
+        datasets: [{
+          data: [5, 10, 15, 20],
+          borderColor: '#0d6efd',
+          backgroundColor: 'rgba(13, 110, 253, 0.2)',
+          tension: 0.4, // bikin garis melengkung
+          pointBackgroundColor: '#0d6efd',
+          pointRadius: 5
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+            max: 100
+          }
+        }
+      }
+    });
+  }
+});
